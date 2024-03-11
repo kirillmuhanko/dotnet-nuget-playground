17e0ad12ccb8171e61bbb9e9970442d2df67490a
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2024-03-11 15:38:20 +0200

    use nswag generated client in api consumer

--- nswag/WeatherForecast.Api.Consumer/Controllers/WeatherForecastController.cs
+++ nswag/WeatherForecast.Api.Consumer/Controllers/WeatherForecastController.cs
@@ -1,7 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using WeatherForecast.Api.Client.Generated;
 
 namespace WeatherForecast.Api.Consumer.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase;
+public class WeatherForecastController(IWeatherForecastClient weatherForecastClient) : ControllerBase
+{
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<Client.Generated.WeatherForecast>> Get()
+    {
+        var forecasts = await weatherForecastClient.GetAsync();
+        return forecasts;
+    }
+}
--- nswag/WeatherForecast.Api.Consumer/Program.cs
+++ nswag/WeatherForecast.Api.Consumer/Program.cs
@@ -1,3 +1,5 @@
+using WeatherForecast.Api.Client.Generated;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IWeatherForecastClient, WeatherForecastClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7113/");
+});
 
 var app = builder.Build();
 
